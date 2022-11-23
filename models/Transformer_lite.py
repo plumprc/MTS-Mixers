23dd0c39a5d3159b331df06de194960217d9e8e3
plumprc <898020464@qq.com>
2022-11-23 23:41:50 +0800

    Transformer-lite: fixed

--- models/Transformer_lite.py
+++ models/Transformer_lite.py
@@ -23,12 +23,12 @@ class Model(nn.Module):
 
         self.projection_channel = nn.Linear(configs.d_model, configs.c_out)
         self.projection_temporal = nn.Linear(configs.seq_len, configs.pred_len)
-        self.rev = RevIN(configs.c_out) if configs.rev else None
+        self.rev = RevIN(configs.enc_in) if configs.rev else None
 
     def forward(self, x):
         x = self.rev(x, 'norm') if self.rev else x
 
-        x = self.enc_embedding(x)
+        x = self.enc_embedding(x, None)
         x, attns = self.encoder(x)
         x = self.projection_channel(x)
         x = self.projection_temporal(x.transpose(1, 2)).transpose(1, 2)
